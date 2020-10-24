bb02dcf314c3de90b08ad3bb0d894e81a7fa253d
piyx <razorabhishek007@gmail.com>
2020-10-24 18:13:47 +0530

    rewrite

--- README.md
+++ README.md
@@ -1,42 +1,27 @@
-# create-repo-automatically  
+# create_repo
+Create repo directly from command line
 
-### Works only for WINDOWS  
 
-## How to use
-1.Download create.exe (link -> https://github.com/piyx/create-repo-automatically/raw/master/create.exe)   
-2.Set environment variables  
-3.Open cmd prompt in the location where create.exe was downloaded  
-4.Type "create github_repo_name" in cmd  
-5.This will create a repository and create a folder in the path specified in environment variables  
 
-## How to set environment variables
+## Usage
+`Note: This will create repo in the same folder from where its run.`
+`python create.py <repo_name> [-private]`
 
-![](images/envrmnt.png)
+```
+positional arguments:
+  repo_name         A name for new repo
 
-### Create 3 varibles  
-1.FILEPATH - path where you want your repo folder to be created  
-2.USER - your github username  
-3.PASS - your github password  
+optional arguments:
+  -h, --help        show this help message and exit
+  -private PRIVATE  Set private to True for private repo [default=False]
+```
 
-## How to run
+## Example
+`python create.py some_random_repo -private=True`
 
-![](images/cmd.png)  
+## Pics
+![](imgs/run.png)
 
-1.Open cmd where create.exe is located  
-2.Type "create your_repo_name"
 
-That's it!!
-
-## Screenshots
-
-![](images/create_exe.png)  
-#### -File create.exe in Desktop (You can put it anywhere you want)  
-
-![](images/path.png)  
-#### -Folder created in path  
-
-![](images/repo_folder.png)  
-#### -Inside repo folder  
-
-![](images/vsc.png)  
-#### -Visual studio code opens and you can start coding immidiately
+## Result
+![](imgs/op.png)
--- create.py
+++ create.py
@@ -1,45 +1,51 @@
-# Inspired by Kalle Hallden
-
 import os
 import sys
+import argparse
 from github import Github
 
-path = os.getenv("FILEPATH") + '\\'
-username = os.getenv("USER")
-password = os.getenv("PASS")
+token = os.getenv("GITHUB_TOKEN")
+g = Github(token)
 
-# Github instance
-g = Github(username, password)
+
 user = g.get_user()
 
-name = sys.argv[1]
-loc = path + name
+parser = argparse.ArgumentParser()
+parser.add_argument('repo_name', type=str, help="A name for new repo")
+parser.add_argument('-private', type=bool, default=False,
+                    help="Set private to True for private repo")
+
+args = parser.parse_args()
+name = args.repo_name
+is_private = args.private
+
+# Current location
+curr = os.getcwd()
 
 
 try:
-    repo = user.create_repo(name)
+    repo = user.create_repo(name, private=is_private)
+    print(repo)
     commands = [f'echo # {repo.name} >> README.md',
                 'git init',
                 'git add README.md',
                 'git commit -m "first commit"',
-                f'git remote add origin https://github.com/{username}/{repo.name}.git',
+                f'git remote add origin https://github.com/{user.login}/{repo.name}.git',
                 'git push -u origin master']
 
     # Create folder with repo name
-    os.mkdir(loc)
+    os.mkdir(repo.name)
 
     # Change directory
-    os.chdir(loc)
+    os.chdir(repo.name)
 
     for command in commands:
         os.system(command)
 
-    print(f"{name} created sucessfully...")
+    print(f"Repo {repo.name} created sucessfully...")
     print("Happy coding!")
 
     # To start coding in vsc
     os.system('code .')
 
-except Exception as e:
-    print("Repo already exists or usernname of password do not match...")
-    print(e)
+except:
+    print("Repo already exists or Invalid token")
