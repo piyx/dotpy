489a2585d36c5b11ba2bee1d7e761cc9d09aa10d
piyx <razorabhishek007@gmail.com>
2020-10-26 23:26:08 +0530

    added files

---
+++ test.py
@@ -0,0 +1,20 @@
+import speedtest
+import datetime
+import time
+import csv
+
+s = speedtest.Speedtest()
+
+
+with open('monitor.csv', 'w', newline='') as csvfile:
+    fieldnames = ['time', 'downspeed', 'upspeed']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    while True:
+        day = datetime.datetime.now()
+        time_ = datetime.datetime.now().strftime("%H:%M")
+        downspeed = round(s.download()/1024**2, 2)
+        upspeed = round(s.upload()/1024**2, 2)
+        writer.writerow(
+            {'time': time_, 'downspeed': downspeed, 'upspeed': upspeed})
